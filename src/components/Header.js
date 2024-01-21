7a4654494dd88c7dfe86f78bed5ea9ba659428ce
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 00:59:27 +0530

    header created

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from "../assets/Main_logo.png"
+const Header = () => {
+    return (
+        <div className='bg-gradient-to-b from-[#434242] '>
+            <img src={logo} alt='logo' width={200} />
+        </div>
+    )
+}
+
+export default Header
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Header from './Header'
 const Login = () => {
   return (
     <div>
-      login
+      <Header/>
     </div>
   )
 }
