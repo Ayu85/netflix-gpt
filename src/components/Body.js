8434ae814d5678f0e9bef67d5918d1a62c5f3068
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 00:51:23 +0530

    react router dom installed and config.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Login from './Login'
 import Browse from './Browse'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+const approuter = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />
+    },
+    {
+        path: '/browse',
+        element: <Browse />
+    }
+])
 const Body = () => {
     return (
         <div>
-            <Login />
-            <Browse />
+            <RouterProvider router={approuter}/>
         </div>
     )
 }
