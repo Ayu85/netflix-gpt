3965f7559def14c3f331c4e4a42c6bb7ffeed37c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 01:04:46 +0530

    login page getting ready

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from "../assets/Main_logo.png"
 const Header = () => {
     return (
-        <div className='bg-gradient-to-b from-[#434242] '>
+        <div className='bg-gradient-to-b from-black w-full absolute z-50 '>
             <img src={logo} alt='logo' width={200} />
         </div>
     )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import bgLogo from "../assets/Bg.jpg"
 const Login = () => {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
+    return (
+        <div className='relative'>
+            <Header />
+            <div className='absolute'
+            ><img src={bgLogo} alt='logo' />
+            </div>
+        </div>
+    )
 }
 
 export default Login
