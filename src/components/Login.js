a3170124d26d1933dd75245c3b1e10844468c571
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 01:33:25 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,8 +6,15 @@ const Login = () => {
         <div className='relative'>
             <Header />
             <div className='absolute'
-            ><img src={bgLogo} alt='logo' className='md:h-[100vw] sm:h-[100vw] lg:h-auto object-fill w-full' />
+            ><img src={bgLogo} alt='logo' className=' md:h-[100vw] sm:h-[100vw] lg:h-auto object-fill w-full' />
             </div>
+            <div className='absolute bg-[#0f0f0f93] flex flex-col gap-4 px-20 py-4 left-[50%] -translate-x-[50%] translate-y-[50%]  z-50 '>
+                <h1 className='text-4xl pb-7 text-white'>Sign in</h1>
+                <input type='mail' className='w-96 h-14  rounded-md bg-slate-50 pl-2' />
+                <input type='password' className='w-96 h-14 rounded-md bg-slate-50 pl-2 text-lg' />
+
+            </div>
+
         </div>
     )
 }
