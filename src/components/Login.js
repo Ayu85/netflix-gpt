1883baed1b6bd4dd6ca4bb8c305b3c9669dd2da1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 21:26:45 +0530

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
             <div className='absolute bg-[#0f0f0fe7] flex flex-col gap-4 px-20 py-4 left-[50%] -translate-x-[50%] translate-y-[50%]  z-50 '>
                 <h1 className='text-3xl pb-4 text-white'>Sign in</h1>
                 <input type='mail' placeholder='Email or phone number' className='w-[320px] h-[50px]  rounded-md bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
-                <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md bg-[#333333] outline-none pl-2 border-b-orange-400 tracking-wider' />
+                <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
                 <button className='bg-[#E50914] w-[320px] h-[50px] rounded-md text-white text-lg'>Sign In</button>
             </div>
 
