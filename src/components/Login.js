9449e4d286119c828f0c11e9f2d4f05ce980c40b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 22:57:16 +0530

    password toogler

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,10 @@ import Footer from './Footer'
 import { FaEye } from "react-icons/fa6";
 const Login = () => {
     const [showPass, setShowPass] = useState(false)
+    const tooglePassword = () => {
+        setShowPass(!showPass)
+    }
+    console.log(showPass);
     return (
         <div className='relative lg:h-[70vw] md:h-[130vw] sm:h-[150vw]'>
             <Header />
@@ -14,8 +18,9 @@ const Login = () => {
             <div className='absolute bg-[#000000d8] flex flex-col gap-4 h-[600px] px-20 py-20 left-[50%] -translate-x-[50%] mt-28 z-50  '>
                 <h1 className='text-3xl pb-4 font-semibold text-white'>Sign in</h1>
                 <input type='mail' placeholder='Email or phone number' className='w-[320px] h-[50px]  rounded-md placeholder-shown:bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
-                <div><input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
-                    <FaEye />
+                <div className='relative'>
+                    <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
+                    <FaEye className='text-[#949494] text-2xl absolute right-3 top-3 cursor-pointer ' onClick={tooglePassword} />
                 </div>
                 <button className='bg-[#E50914] w-[320px] h-[50px] mt-7 rounded-md text-white text-lg'>Sign In</button>
                 <div className='flex justify-between w-[320px] h-[50px] -mt-5 text-sm font-semibold'>
