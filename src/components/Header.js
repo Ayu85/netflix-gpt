9aebd719087d5a3a2f9d9b0dfe270005dc473a71
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 22:11:26 +0530

    login pafe styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 import logo from "../assets/Main_logo.png"
 const Header = () => {
     return (
-        <div className='bg-gradient-to-b from-black w-full absolute z-50 '>
-            <img src={logo} alt='logo' width={200} />
+        <div className='bg-gradient-to-b from-black w-full absolute z-50 pl-2 '>
+            <img src={logo} alt='logo' width={220} />
         </div>
     )
 }
