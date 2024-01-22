15c931ccb6735fb343a3db7aa9573471843e34c3
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 22:53:14 +0530

    password show hide

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import bgLogo from "../assets/Bg.jpg"
 import Footer from './Footer'
+import { FaEye } from "react-icons/fa6";
 const Login = () => {
+    const [showPass, setShowPass] = useState(false)
     return (
         <div className='relative lg:h-[70vw] md:h-[130vw] sm:h-[150vw]'>
             <Header />
@@ -12,7 +14,9 @@ const Login = () => {
             <div className='absolute bg-[#000000d8] flex flex-col gap-4 h-[600px] px-20 py-20 left-[50%] -translate-x-[50%] mt-28 z-50  '>
                 <h1 className='text-3xl pb-4 font-semibold text-white'>Sign in</h1>
                 <input type='mail' placeholder='Email or phone number' className='w-[320px] h-[50px]  rounded-md placeholder-shown:bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
-                <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
+                <div><input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
+                    <FaEye />
+                </div>
                 <button className='bg-[#E50914] w-[320px] h-[50px] mt-7 rounded-md text-white text-lg'>Sign In</button>
                 <div className='flex justify-between w-[320px] h-[50px] -mt-5 text-sm font-semibold'>
                     <div className='text-[#ADADAD] flex items-center gap-1'>
@@ -26,7 +30,7 @@ const Login = () => {
                         <span className='text-blue-500'>Learn more</span></h1>
                 </div>
             </div>
-           <Footer />
+            <Footer />
         </div>
     )
 }
