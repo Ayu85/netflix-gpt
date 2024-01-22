ee385f6cc62d58545faac82242ff014e9ac661cb
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 21:24:42 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,11 +8,11 @@ const Login = () => {
             <div className='absolute'
             ><img src={bgLogo} alt='logo' className=' md:h-[100vw] sm:h-[100vw] lg:h-auto object-fill w-full' />
             </div>
-            <div className='absolute bg-[#0f0f0fd8] flex flex-col gap-4 px-20 py-4 left-[50%] -translate-x-[50%] translate-y-[50%]  z-50 '>
+            <div className='absolute bg-[#0f0f0fe7] flex flex-col gap-4 px-20 py-4 left-[50%] -translate-x-[50%] translate-y-[50%]  z-50 '>
                 <h1 className='text-3xl pb-4 text-white'>Sign in</h1>
-                <input type='mail' className='w-80 h-12  rounded-md bg-slate-50 pl-2' />
-                <input type='password' className='w-80 h-12 rounded-md bg-slate-50 pl-2 text-lg' />
-                <button className='bg-[#E50914] w-80 h-12 rounded-md text-white text-lg'>Sign In</button>
+                <input type='mail' placeholder='Email or phone number' className='w-[320px] h-[50px]  rounded-md bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
+                <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md bg-[#333333] outline-none pl-2 border-b-orange-400 tracking-wider' />
+                <button className='bg-[#E50914] w-[320px] h-[50px] rounded-md text-white text-lg'>Sign In</button>
             </div>
 
         </div>
