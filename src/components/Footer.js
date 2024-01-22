a45ec49d4dcd59aeba4d89128330efb5fd82deac
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 22:40:32 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-[#000000d8] text-[#ADADAD] absolute bottom-0 ' >
+        <div className='bg-[#000000d8] gap-10 text-[#ADADAD] absolute bottom-0 w-full px-4 py-8 flex flex-col  items-center ' >
             <div>Questions? Call 000-800-122-355</div>
-            <div >
-                <div>
+            <div className='flex gap-28'>
+                <div className='flex flex-col gap-5'>
                     <h1>FAQ</h1>
                     <h1>Cookies prefrences</h1>
                 </div>
-                <div>
+                <div className='flex flex-col gap-5'>
                     <h1>Help center</h1>
                     <h1>Corporate information</h1>
                 </div>
-                <div>
+                <div >
                     <h1>Terms of use</h1>
                 </div>
                 <div>
