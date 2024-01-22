656ba545293858e297f26499273b9e7b979d869e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-23 00:42:38 +0530

    form validation done

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,9 @@ import { FaEyeLowVision } from "react-icons/fa6";
 import validate from '../utils/validate';
 const Login = () => {
     const [showPass, setShowPass] = useState(false)
+    const [emailvalidationError, setEmailValidationError] = useState("")
+    const [passwordvalidationError, setPasswordValidationError] = useState("")
+
     const email = useRef(null);
     const password = useRef(null);
     const tooglePassword = () => {
@@ -17,9 +20,14 @@ const Login = () => {
         console.log(password?.current?.value);
         console.log(validate(email?.current?.value, password?.current?.value)
         );
+        const validateInfo = validate(email?.current?.value, password?.current?.value);
+        setEmailValidationError(validateInfo?.emailError)
+        console.log(emailvalidationError);
+        setPasswordValidationError(validateInfo?.passwordError);
+        console.log(passwordvalidationError);
+
     }
-    // console.log(showPass);
-    console.log(email);
+
     return (
         <div className='relative lg:h-[70vw] md:h-[130vw] sm:h-[150vw]'>
             <Header />
@@ -31,12 +39,14 @@ const Login = () => {
             }} className='absolute bg-[#000000d8] flex flex-col gap-4 h-[600px] px-20 py-20 left-[50%] -translate-x-[50%] mt-28 z-50  '>
                 <h1 className='text-3xl pb-4 font-semibold text-white'>Sign in</h1>
                 <input type='mail' placeholder='Email or phone number' ref={email} className='w-[320px] h-[50px]  rounded-md placeholder-shown:bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
+                <span className='text-[#E87C03] -mt-3 ml-1'>{emailvalidationError}</span>
                 <div className='relative'>
                     <input type={!showPass ? "password" : "text"} placeholder='Password' ref={password}
                         className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
                     {!showPass ? <FaEye className='text-[#949494] text-2xl absolute right-3 top-3 cursor-pointer ' onClick={tooglePassword} />
                         : <FaEyeLowVision className='text-[#949494] text-2xl absolute right-3 top-3 cursor-pointer ' onClick={tooglePassword} />
                     }</div>
+                <span className='text-[#E87C03] -mt-3 ml-1'>{passwordvalidationError}</span>
                 <input type="submit" onClick={handleSumbmit} className='bg-[#E50914] w-[320px] h-[50px] mt-7 rounded-md text-white cursor-pointer text-lg' />
                 <div className='flex justify-between w-[320px] h-[50px] -mt-5 text-sm font-semibold'>
                     <div className='text-[#ADADAD] flex items-center gap-1'>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,12 +1,14 @@
 
 const validate = (email, passowrd) => {
+    let emailError = '';
+    let passwordError = '';
     var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    if (!email.includes("@gmail.com"))
-        return "Invalid email"
-    else if ( !regularExpression.test(passowrd))
-        return "Invalid password"
-    else
-        return null;
+    if (!email?.includes("@gmail.com"))
+        emailError = "Invalid email"
+    if (!regularExpression.test(passowrd))
+        passwordError = "Invalid password"
+
+    return { emailError, passwordError }
 
 }
 
