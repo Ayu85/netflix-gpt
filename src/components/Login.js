a5f590cb964d088372a988d3f9d1e6e2aec9c56b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 21:37:58 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,16 @@ const Login = () => {
             ><img src={bgLogo} alt='logo' className=' md:h-[100vw] sm:h-[100vw] lg:h-auto object-fill w-full' />
             </div>
             <div className='absolute bg-[#0f0f0fe7] flex flex-col gap-4 px-20 py-4 left-[50%] -translate-x-[50%] translate-y-[50%]  z-50 '>
-                <h1 className='text-3xl pb-4 text-white'>Sign in</h1>
-                <input type='mail' placeholder='Email or phone number' className='w-[320px] h-[50px]  rounded-md bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
-                <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
-                <button className='bg-[#E50914] w-[320px] h-[50px] rounded-md text-white text-lg'>Sign In</button>
+                <h1 className='text-3xl pb-4 font-semibold text-white'>Sign in</h1>
+                <input type='mail' placeholder='Email or phone number' className='w-[320px] h-[50px]  rounded-md placeholder-shown:bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
+                <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
+                <button className='bg-[#E50914] w-[320px] h-[50px] mt-7 rounded-md text-white text-lg'>Sign In</button>
+                <div>
+                    <div className='text-[#737373] flex items-center gap-1'>
+                        <input type='checkbox' className='appearance-none default:ring-2 ring-white ' /><label>Remember me</label>
+                    </div>
+                    <div></div>
+                </div>
             </div>
 
         </div>
