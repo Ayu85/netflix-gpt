35ad64cfd286d3b112e62e066e1573a9bbdd43fb
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 22:26:28 +0530

    footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,32 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-      <div>Questions? Call 000-800-122-355</div>
-      <div className='bg-[#000000d8]'>
+    return (
+        <div className='bg-[#000000d8] text-[#ADADAD]' >
+            <div>Questions? Call 000-800-122-355</div>
+            <div >
+                <div>
+                    <h1>FAQ</h1>
+                    <h1>Cookies prefrences</h1>
+                </div>
+                <div>
+                    <h1>Help center</h1>
+                    <h1>Corporate information</h1>
+                </div>
+                <div>
+                    <h1>Terms of use</h1>
+                </div>
+                <div>
+                    <h1>Privacy</h1>
+                </div>
 
-      </div>
-      <div>
-        <select />
-      </div>
+            </div>
+            <div>
+                <select />
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Footer
