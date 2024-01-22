6f87f5028d91932acb9e7ab2e21009c1cb5654ed
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-22 21:59:19 +0530

    login form ready

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,16 +8,21 @@ const Login = () => {
             <div className='absolute'
             ><img src={bgLogo} alt='logo' className=' md:h-[100vw] sm:h-[100vw] lg:h-auto object-fill w-full' />
             </div>
-            <div className='absolute bg-[#0f0f0fe7] flex flex-col gap-4 px-20 py-4 left-[50%] -translate-x-[50%] translate-y-[50%]  z-50 '>
+            <div className='absolute bg-[#000000d8] flex flex-col gap-4 h-[600px] px-20 py-20 left-[50%] -translate-x-[50%] mt-20 z-50 '>
                 <h1 className='text-3xl pb-4 font-semibold text-white'>Sign in</h1>
                 <input type='mail' placeholder='Email or phone number' className='w-[320px] h-[50px]  rounded-md placeholder-shown:bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
                 <input type='password' placeholder='Password' className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
                 <button className='bg-[#E50914] w-[320px] h-[50px] mt-7 rounded-md text-white text-lg'>Sign In</button>
-                <div>
-                    <div className='text-[#737373] flex items-center gap-1'>
+                <div className='flex justify-between w-[320px] h-[50px] -mt-5 text-sm font-semibold'>
+                    <div className='text-[#ADADAD] flex items-center gap-1'>
                         <input type='checkbox' className='  peer-checked:bg-slate-900 ' /><label>Remember me</label>
                     </div>
-                    <div></div>
+                    <div className='text-[#ADADAD] flex items-center gap-1'>Need Help?</div>
+                </div>
+                <div className='flex flex-col gap-3 mt-5 w-[320px] text-[#ADADAD] h-[50px]'>
+                    <h1>New to Netflix?<span className='text-white pl-2'>Sign up now</span></h1>
+                    <h1 className='text-sm'>This page is protected by Google reCAPTCHA to ensure you're not a bot.
+                        <span className='text-blue-500'>Learn more</span></h1>
                 </div>
             </div>
 
