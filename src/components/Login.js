a93221643efaf00e92dcd7e56e218c9507364363
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-23 01:05:33 +0530

    sign in and sign up toogler

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,23 +9,19 @@ const Login = () => {
     const [showPass, setShowPass] = useState(false)
     const [emailvalidationError, setEmailValidationError] = useState("")
     const [passwordvalidationError, setPasswordValidationError] = useState("")
-
+    const [isSignIn, setSignIn] = useState(true);
     const email = useRef(null);
     const password = useRef(null);
     const tooglePassword = () => {
         setShowPass(!showPass)
     }
+    const toogleSignIn = () => {
+        setSignIn(!isSignIn)
+    }
     const handleSumbmit = () => {
-        console.log(email?.current?.value);
-        console.log(password?.current?.value);
-        console.log(validate(email?.current?.value, password?.current?.value)
-        );
         const validateInfo = validate(email?.current?.value, password?.current?.value);
         setEmailValidationError(validateInfo?.emailError)
-        console.log(emailvalidationError);
         setPasswordValidationError(validateInfo?.passwordError);
-        console.log(passwordvalidationError);
-
     }
 
     return (
@@ -34,7 +30,7 @@ const Login = () => {
             <div className='absolute'
             ><img src={bgLogo} alt='logo' className=' md:h-[130vw] sm:h-[150vw] bg-cover lg:h-[70vw]' />
             </div>
-            <form onSubmit={(e) => {
+            {isSignIn && <form onSubmit={(e) => {
                 e.preventDefault()
             }} className='absolute bg-[#000000d8] flex flex-col gap-4 h-[600px] px-20 py-20 left-[50%] -translate-x-[50%] mt-28 z-50  '>
                 <h1 className='text-3xl pb-4 font-semibold text-white'>Sign in</h1>
@@ -55,11 +51,41 @@ const Login = () => {
                     <div className='text-[#ADADAD] flex items-center gap-1'>Need Help?</div>
                 </div>
                 <div className='flex flex-col gap-3 mt-5 w-[320px] text-[#ADADAD] h-[50px]'>
-                    <h1>New to Netflix?<span className='text-white pl-2'>Sign up now</span></h1>
+                    <h1>New to Netflix?<span className='text-white pl-2 cursor-pointer' onClick={toogleSignIn}>Sign up now</span></h1>
                     <h1 className='text-sm'>This page is protected by Google reCAPTCHA to ensure you're not a bot.
                         <span className='text-blue-500'>Learn more</span></h1>
                 </div>
             </form>
+            }
+            {
+                !isSignIn && <form onSubmit={(e) => {
+                    e.preventDefault()
+                }} className='absolute bg-[#000000d8] flex flex-col gap-4 h-[600px] px-20 py-20 left-[50%] -translate-x-[50%] mt-28 z-50  '>
+                    <h1 className='text-3xl pb-4 font-semibold text-white'>Sign up</h1>
+                    <input type='text' placeholder='User name' ref={email} className='w-[320px] h-[50px]  rounded-md placeholder-shown:bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
+                    <input type='mail' placeholder='Email or phone number' ref={email} className='w-[320px] h-[50px]  rounded-md placeholder-shown:bg-[#333333] outline-none border-b-orange-400 border-b pl-2 tracking-wider' />
+                    <span className='text-[#E87C03] -mt-3 ml-1'>{emailvalidationError}</span>
+                    <div className='relative'>
+                        <input type={!showPass ? "password" : "text"} placeholder='Password' ref={password}
+                            className='w-[320px] h-[50px] rounded-md  placeholder-shown:bg-[#333333] outline-none pl-2 border-b-orange-400 border-b tracking-wider' />
+                        {!showPass ? <FaEye className='text-[#949494] text-2xl absolute right-3 top-3 cursor-pointer ' onClick={tooglePassword} />
+                            : <FaEyeLowVision className='text-[#949494] text-2xl absolute right-3 top-3 cursor-pointer ' onClick={tooglePassword} />
+                        }</div>
+                    <span className='text-[#E87C03] -mt-3 ml-1'>{passwordvalidationError}</span>
+                    <input type="submit" onClick={handleSumbmit} className='bg-[#E50914] w-[320px] h-[50px] mt-7 rounded-md text-white cursor-pointer text-lg' />
+                    <div className='flex justify-between w-[320px] h-[50px] -mt-5 text-sm font-semibold'>
+                        <div className='text-[#ADADAD] flex items-center gap-1'>
+                            <input type='checkbox' className='  peer-checked:bg-slate-900 ' /><label>Remember me</label>
+                        </div>
+                        <div className='text-[#ADADAD] flex items-center gap-1'>Need Help?</div>
+                    </div>
+                    <div className='flex flex-col gap-3 mt-5 w-[320px] text-[#ADADAD] h-[50px]'>
+                        <h1>Already have account?<span className='text-white pl-2 cursor-pointer' onClick={toogleSignIn}>Sign In</span></h1>
+                        <h1 className='text-sm'>This page is protected by Google reCAPTCHA to ensure you're not a bot.
+                            <span className='text-blue-500'>Learn more</span></h1>
+                    </div>
+                </form>
+            }
             <Footer />
         </div>
     )
