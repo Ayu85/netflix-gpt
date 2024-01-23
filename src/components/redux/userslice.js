7aa3e6e0f1fe6f1043520411f4044cd8c54ca8bb
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 00:16:47 +0530

    userslice created

--- src/components/redux/userslice.js
+++ src/components/redux/userslice.js
@@ -1,5 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const userslice=createSlice({
+const userslice = createSlice({
+    name: "user-details",
+    initialState: {
+        userData: {}
+    },
+    reducers: {
+        addUser: (state, action) => {
+            state.userData = action.payload;
+        }
+    }
 
 })
+export const { addUser } = userslice.actions;
+export default userslice.reducer;
