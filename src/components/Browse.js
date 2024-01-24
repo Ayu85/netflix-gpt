572786b68a123d0ad844d185b33fbc4e912cb6f2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 20:00:59 +0530

    time to use the tmdb api

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,11 +4,12 @@ import bgLogo from "../assets/Bg.jpg"
 
 const Browse = () => {
     return (
-        <div className='relative'>
+        <div className='relative '>
             <BrowseHeader />
             <div className='absolute'
             ><img src={bgLogo} alt='logo' className='filter brightness-50 md:h-[130vw] sm:h-[150vw] bg-cover lg:h-[70vw]' />
             </div>
+       
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import logo from "../assets/Main_logo.png"
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { FcOk } from "react-icons/fc";
+import { FcBusinessman } from "react-icons/fc";
+import { CiLogout } from "react-icons/ci";
+
 
 const Header = () => {
     return (
@@ -15,10 +19,10 @@ export const BrowseHeader = () => {
     const [showUserData, setShowUser] = useState(false)
     const navigate = useNavigate();
     const userData = useSelector(store => store.user.userData)
-    return <div className='bg-gradient-to-b from-black w-full absolute flex justify-between z-50 pl-2 '>
+    return <div className=' overflow-x-hidden h-28 bg-gradient-to-b from-black w-full absolute flex justify-between z-50 pl-2 '>
         <div className='pl-5 pt-2'> <img src={logo} alt='logo' className='w-48 md:w-48 sm:w-32' /></div>
-        <div className='relative'><button className=' bg-[#E50914] px-3 h-8
-         rounded-md font-semibold mr-20 mt-7  text-white z-50 sm:mt-5' onClick={() => {
+        <div className='relative h-20'><button className=' bg-[#E50914] px-3 h-8
+         rounded-md font-semibold mr-20 mt-7 flex items-center gap-1 text-white z-50 sm:mt-5' onClick={() => {
                 const auth = getAuth();
                 signOut(auth).then(() => {
                     // Sign-out successful.
@@ -32,8 +36,13 @@ export const BrowseHeader = () => {
                 setShowUser(true)
             }} onMouseLeave={() => {
                 setShowUser(false)
-            }}>Sign out</button>
-            {showUserData && <div className='text-white z-50 absolute right-0 bg-[#232323] px-2 py-2 mr-2'>USER: {userData.email}</div>
+            }}><CiLogout  className='text-white font-bold' />
+            Sign out</button>
+            {
+                showUserData && < div className='text-white z-50 absolute right duration-100 
+             transition-all bg-[#232323] px-3 py-2 mr-2 flex items-center -left-10 top-14 gap-2'><FcBusinessman />
+                    {userData.email} <FcOk />
+                </div>
             }        </div>
 
     </div>
