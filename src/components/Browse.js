87f8f52d9b1724eb16168f76977939e0cb185ae4
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-25 00:07:23 +0530

    start designing the browse movies page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,11 +4,8 @@ import bgLogo from "../assets/Bg.jpg"
 
 const Browse = () => {
     return (
-        <div className='relative '>
+        <div>
             <BrowseHeader />
-            <div className='absolute'
-            ><img src={bgLogo} alt='logo' className='filter brightness-50 md:h-[130vw] sm:h-[150vw] bg-cover lg:h-[70vw]' />
-            </div>
         </div>
     )
 }
