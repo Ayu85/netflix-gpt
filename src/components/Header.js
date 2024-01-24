5a2fe4d0bf1bdc77940fccb65d2edab3a486a12b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 19:06:14 +0530

    browse page

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
 export const BrowseHeader = () => {
     return <div className='bg-gradient-to-b from-black w-full absolute flex justify-between z-50 pl-2 '>
         <div className='pl-5 pt-2'> <img src={logo} alt='logo' className='w-48 md:w-48 sm:w-32' /></div>
-        <button className=' bg-[#E50914] px-2 py-1 h-9 rounded-md mr-20 mt-7  text-white z-50 font-semibold'>Sign out</button>
+        <button className=' bg-[#E50914] px-2 h-8 rounded-md mr-20 mt-7  text-white z-50 font-semibold'>Sign out</button>
     </div>
 }
 
