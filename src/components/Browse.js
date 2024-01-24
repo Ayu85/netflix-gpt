3f39dcd753237ae4e19136dca2f54d92984c1a5f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-24 19:02:31 +0530

    browsing page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { BrowseHeader } from './Header'
+import bgLogo from "../assets/Bg.jpg"
 const Browse = () => {
     return (
         <div>
             <BrowseHeader/>
+            <div className='absolute'
+            ><img src={bgLogo} alt='logo' className='filter brightness-50 md:h-[130vw] sm:h-[150vw] bg-cover lg:h-[70vw]' />
+            </div>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import logo from "../assets/Main_logo.png"
 const Header = () => {
     return (
         <div className='bg-gradient-to-b from-black w-full absolute z-50 pl-2 '>
-            <img src={logo} alt='logo' width={210} />
+            <img src={logo} alt='logo' className='w-48' />
         </div>
     )
 }
 export const BrowseHeader = () => {
-    return <div >
-        < Header  />
-        <button className='absolute right-0 text-white z-50 font-semibold'>Sign out</button>
+    return <div className='bg-gradient-to-b from-black w-full absolute flex justify-between z-50 pl-2 '>
+        <div className='pl-5 pt-2'> <img src={logo} alt='logo' className='w-48 md:w-48 sm:w-32' /></div>
+        <button className=' bg-[#E50914] px-2 py-1 h-9 rounded-md mr-20 mt-7  text-white z-50 font-semibold'>Sign out</button>
     </div>
 }
 
