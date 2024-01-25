4b7ba7fa9afcc9b1035b764673db49f4397ac978
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-25 18:30:36 +0530

    video bg page

---
+++ src/components/Browse Page/VIdeoBackground.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const VideoBackground = () => {
+    return (
+        <div>
+            <div>
+                {/* video player */}
+            </div>
+            <div>
+                {/* video title and info */}
+            </div>
+
+        </div>
+    )
+}
+
+export default VideoBackground
