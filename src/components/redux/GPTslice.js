49a660dce880919b65fd64fc36432133dbbf5234
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-27 00:25:36 +0530

    gpt slice created

---
+++ src/components/redux/GPTslice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const GPTslice = createSlice({
+    name: "gptslice",
+    initialState: {
+        value: false
+    },
+    reducers: {
+        toogleGPT: (state, action) => {
+            state.value = !state.value
+        }
+    }
+})
+export const { toogleGPT } = GPTslice.actions;
+export default GPTslice
