5af702194d4d4180196715c60d030e9288b5f936
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-26 23:46:00 +0530

    working 100%

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -4,7 +4,7 @@ import { CiCircleInfo } from "react-icons/ci";
 
 const VideoTitle = ({ title, overview }) => {
     return (
-        <div className=' items-center bg-gradient-to-r from-[#0f0f0f61] flex absolute  text-white top-0   pl-5 aspect-video w-screen'>
+        <div className=' items-center bg-gradient-to-r from-[#0f0f0f9b] flex absolute  text-white top-0   pl-5 aspect-video w-screen'>
             <div className='lg:max-w-96 md:max-w-44  '>
                 <h1 className='lg:text-4xl font-bold mb-5 md:text-2xl'>{title}</h1>
                 <h1 className=' hidden lg:flex'>{overview}</h1>
