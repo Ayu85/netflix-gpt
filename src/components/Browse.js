e58fd84a71920df1c9acb1875a6d9f1b36db7300
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-27 00:42:23 +0530

    gpt search created

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -7,17 +7,20 @@ import SecondaryContainer from './SecondaryContainer';
 import usePopularMovies from '../hooks/usePopularMovies';
 import useTopRatedMovies from '../hooks/useTopRatedMovies';
 import useUpcomingMovies from '../hooks/useUpcomingMovies';
+import GPTsearch from './GPTsearch';
 
 const Browse = () => {
     useNowPlayingMovies();
     usePopularMovies();
     useTopRatedMovies();
     useUpcomingMovies();
+    const showGPT = useSelector(store => store.gpt.value)
+
     return (
         <div className='relative bg-zinc-900'>
             <BrowseHeader />
-            <MainContainer />
-            <SecondaryContainer />
+            {showGPT ? <GPTsearch /> : <><MainContainer />
+                <SecondaryContainer /></>}
         </div>
     )
 }
