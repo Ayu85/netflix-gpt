d73ff25d340245d564cb630deb3e2e02ef13e72b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-27 00:26:04 +0530

    slice provided to store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userslice from "./userslice";
 import movieslice from "./movieslice";
+import GPTslice from "./GPTslice";
 const store = configureStore({
     reducer: {
         user: userslice,
-        movie: movieslice
+        movie: movieslice,
+        gpt: GPTslice
     }
 })
 export default store;
