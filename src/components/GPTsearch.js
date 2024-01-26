beea964dd82988fbc04a39511bb93ae450495372
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-27 00:55:40 +0530

    gpt page designing

--- src/components/GPTsearch.js
+++ src/components/GPTsearch.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import bgLogo from "../assets/Bg.jpg"
 
 const GPTsearch = () => {
     return (
-        <div>
-            gpt search
+        <div >
+            <div className='absolute'
+            ><img src={bgLogo} alt='logo' className=' filter brightness-50 md:h-[130vw] sm:h-[150vw] bg-cover lg:h-[70vw]' />
+            </div>
+            <div className='absolute z-50 bg-zinc-800 flex ml-[50%] -translate-x-[50%] mt-[30%] -translate-y-[50%]'>
+                <input type='text' className='w-96 rounded-l-full px-6 py-3 text-xl ml-3 ' />
+                <button className='bg-green-400 text-white px-6 py-3 text-xl rounded-r-full'>Search</button>
+            </div>
         </div>
     )
 }
