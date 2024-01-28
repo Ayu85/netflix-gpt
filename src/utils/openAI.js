4932332ab24249c8761a88c035ce3de1c0e49d6e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-28 16:21:21 +0530

    open ai

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -1,5 +1,6 @@
 import OpenAI from 'openai';
+import { OPEN_AI_KEY } from './constants';
 const openai = new OpenAI({
-    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
-  });
-  export default openai;
+    apiKey: OPEN_AI_KEY, // This is the default and can be omitted
+});
+export default openai;
