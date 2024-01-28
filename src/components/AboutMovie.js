993cf0e09e94c4900fe01582d255d28b8a8a9b76
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-28 22:35:55 +0530

    details fetched

--- src/components/AboutMovie.js
+++ src/components/AboutMovie.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react'
-import Header from './Header'
-import { options, optionsfordetail } from '../utils/constants'
+
 import { useParams } from 'react-router-dom'
 import useMovieDetails from '../hooks/useMovieDetails'
 import AboutMovieHeader from './AboutMovieHeader'
 import MovieDetails from './MovieDetails'
+import { useSelector } from 'react-redux'
 const AboutMovie = () => {
+    const details = useSelector(store => store.moviedetails.details)
     const id = useParams();
     useMovieDetails(id);
+    if (!details) return;
+    console.log(details);
     return (
         <div>
             <AboutMovieHeader />
--- src/hooks/useMovieDetails.js
+++ src/hooks/useMovieDetails.js
@@ -1,12 +1,14 @@
 import { useEffect } from 'react'
 
 import { options } from '../utils/constants';
-
+import { addDetails } from '../components/redux/moviedetailsslice';
+import { useDispatch } from 'react-redux';
 const useMovieDetails = (id) => {
+    const dispatch = useDispatch();
     const getMovieDetails = async () => {
         const rawData = await fetch('https://api.themoviedb.org/3/movie/' + id.id, options)
         const jsonData = await rawData.json();
-        console.log(jsonData);
+        dispatch(addDetails(jsonData))
     }
     useEffect(() => {
         getMovieDetails()
