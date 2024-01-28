81e828b38098af73accda9764d6ff2641812fac7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-28 21:31:36 +0530

    custom hook

--- src/components/AboutMovie.js
+++ src/components/AboutMovie.js
@@ -2,17 +2,10 @@ import React, { useEffect } from 'react'
 import Header from './Header'
 import { options, optionsfordetail } from '../utils/constants'
 import { useParams } from 'react-router-dom'
+import useMovieDetails from '../hooks/useMovieDetails'
 const AboutMovie = () => {
     const id = useParams();
-    console.log(id);
-    const getMovieDetails = async () => {
-        const rawData = await fetch('https://api.themoviedb.org/3/movie/' +  id.id , options)
-        const jsonData = await rawData.json();
-        console.log(jsonData);
-    }
-    useEffect(() => {
-        getMovieDetails()
-    }, [])
+    useMovieDetails(id);
     return (
         <div>
             <>about movie</>
---
+++ src/hooks/useMovieDetails.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { options } from '../utils/constants';
+
+const useMovieDetails = (id) => {
+   
+    const getMovieDetails = async () => {
+        const rawData = await fetch('https://api.themoviedb.org/3/movie/' + id.id, options)
+        const jsonData = await rawData.json();
+        console.log(jsonData);
+    }
+    useEffect(() => {
+        getMovieDetails()
+    }, [])
+}
+
+export default useMovieDetails
