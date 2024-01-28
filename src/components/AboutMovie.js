6533be84793a10c02e835dab7fe4926dd1b7693e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-28 20:08:57 +0530

    movie route added

--- src/components/AboutMovie.js
+++ src/components/AboutMovie.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutMovie = () => {
     return (
         <div>
-
+            about movie
         </div>
     )
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './Login'
 import Browse from './Browse'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AboutMovie from './AboutMovie'
 
 
 const approuter = createBrowserRouter([
@@ -12,6 +13,10 @@ const approuter = createBrowserRouter([
     {
         path: '/browse',
         element: <Browse />
+    },
+    {
+        path: '/about/:id',
+        element:<AboutMovie/>
     }
 ])
 
