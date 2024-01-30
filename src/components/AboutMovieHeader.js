194e243cd7bc7c8fc13cb6527868d7986a928512
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-30 19:02:16 +0530

    netflix gpt done

--- src/components/AboutMovieHeader.js
+++ src/components/AboutMovieHeader.js
@@ -5,7 +5,7 @@ import bgLogo from "../assets/Bg.jpg"
 const AboutMovieHeader = () => {
     return (
         <div className='relative'>
-            <div className='bg-gradient-to-b from-black absolute z-10 w-screen h-screen '>
+            <div className='absolute z-50 w-screen h-screen '>
                 <img src={logo} alt='logo' className='w-44' />
             </div>
             <img src={bgLogo} alt='logo' className='w-screen h-screen absolute filter brightness-50' />
